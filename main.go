22009af4ce52433f0069c1964a26be7b4db34e18
miafate <miapurplefroot@gmail.com>
2024-11-23 01:13:52 -0300

    clase if - switch

--- main.go
+++ main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/miafate/hellogo/variables"
+	"runtime"
 )
 
 func main() {
-	estado, texto := variables.ConvertirATexto(14)
-	fmt.Println(estado)
-	fmt.Println(texto)
+	// estado, texto := variables.ConvertirATexto(14)
+	// fmt.Println(estado)
+	// fmt.Println(texto)
+	if os := runtime.GOOS; os == "linux" || os == "OS X." {
+		fmt.Println("esto no es windows, es", os)
+	} else {
+		fmt.Println("esto es windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("esto es Linux")
+	case "darwin":
+		fmt.Println("esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
