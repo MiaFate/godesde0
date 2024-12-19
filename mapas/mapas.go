55f8b75bd34e43219e3cd8e8540d57aa60def954
miafate <miapurplefroot@gmail.com>
2024-12-19 18:22:12 -0300

    added maps

--- mapas/mapas.go
+++ mapas/mapas.go
@@ -4,5 +4,24 @@ import "fmt"
 
 func MostrarMapas() {
 	paises := make(map[string]string)
-	fmt.Println(paises)
+	// fmt.Println(paises)
+
+	paises["Mexico"] = "D.F."
+	paises["Argentina"] = "Buenos Aires"
+
+	// fmt.Println(paises)
+	// fmt.Println(paises["Argentina"])
+
+	campeonato := map[string]int{
+		"Barcelona":   39,
+		"Real Madrid": 38,
+		"Chivas":      37,
+		"Boca Jrs":    30,
+	}
+	fmt.Println(campeonato)
+	for equipo, puntaje := range campeonato {
+		fmt.Printf("Equipo %s, tiene un puntaje de %d \n", equipo, puntaje)
+	}
+	delete(campeonato, "Real Madrid")
+	fmt.Println(campeonato)
 }
